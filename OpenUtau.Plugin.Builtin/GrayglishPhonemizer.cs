fc9d91fccf26c0d59a8760c75cb3316586cec8e0
Mark Beltran <markmbeltran@gmail.com>
2021-10-23 11:54:10 +0800

    Phonemizer created

---
+++ OpenUtau.Plugin.Builtin/GrayglishPhonemizer.cs
@@ -0,0 +1,20 @@
+﻿using OpenUtau.Api;
+using OpenUtau.Core.Ustx;
+
+namespace OpenUtau.Plugin.Builtin {
+    [Phonemizer("GraySlate X-SAMPA English", "EN Grayglish", "TUBS")]
+    public class GrayglishPhonemizer : Phonemizer {
+        private USinger singer;
+        public override void SetSinger(USinger singer) => this.singer = singer;
+
+        public override Result Process(Note[] notes, Note? prev, Note? next, Note? prevNeighbour, Note? nextNeighbour) {
+            return new Result {
+                phonemes = new Phoneme[] {
+                    new Phoneme() {
+                        phoneme = notes[0].lyric
+                    }
+                }
+            };
+        }
+    }
+}
