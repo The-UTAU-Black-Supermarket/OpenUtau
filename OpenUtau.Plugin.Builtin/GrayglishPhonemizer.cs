4bb34e42b9424601acdc5c64031f2c48b49e0c61
Mark Beltran <markmbeltran@gmail.com>
2021-10-24 10:28:09 +0800

    Multi syllable words without context

--- OpenUtau.Plugin.Builtin/GrayglishPhonemizer.cs
+++ OpenUtau.Plugin.Builtin/GrayglishPhonemizer.cs
@@ -11,21 +11,28 @@ namespace OpenUtau.Plugin.Builtin {
         
 
         public GrayglishPhonemizer() {
+            // Initialize phonemizer with cmudict
             cmudict = G2pDictionary.GetShared("cmudict");
         }
 
+        // Store singer in field for OTO access
         private USinger singer;
         public override void SetSinger(USinger singer) => this.singer = singer;
 
         public override Result Process(Note[] notes, Note? prev, Note? next, Note? prevNeighbour, Note? nextNeighbour) {
-            //var prevSymbols = prevNeighbour == null ? null : GetSymbols(prevNeighbour.Value);
-            //var prevSymbol = prevSymbols == null ? "-" : prevSymbols.Last();
+            // Total length of current note
+            int noteLength = 0;
+            Array.ForEach(notes, i => noteLength += i.duration);
+
+            var prevSymbols = prevNeighbour == null ? null : GetSymbols(prevNeighbour.Value);
+            var prevSymbol = prevSymbols == null ? "-" : prevSymbols.Last();
             var note = notes[0];
             var symbols = GetSymbols(note);
             //var nextSymbols = nextNeighbour == null ? null : GetSymbols(nextNeighbour.Value);
             //var nextSymbol = nextSymbols == null ? "-" : nextSymbols.First();
 
             if (symbols == null || symbols.Length == 0) {
+                // Can't find word in dictionary. Assume user is manually entering aliases.
                 return new Result {
                     phonemes = new Phoneme[] {
                         new Phoneme() {
@@ -48,26 +55,29 @@ namespace OpenUtau.Plugin.Builtin {
             var firstVowel = symbols[firstVowelPos];
 
             if (firstVowelPos == 0) {
-                //var text = "";
-                //if (prevSymbol == "-") {
-                //    text = $"-{vowel}";
-                //} else {
-                //    if (isVowel.ContainsKey(prevSymbol) && isVowel[prevSymbol]) {
-                //        if (prevSymbol == vowel) {
-                //            text = vowel;
-                //        } else {
-                //            text = $"{prevSymbol} {vowel}";
-                //        }
-                //    } else {
-                //        text = $"{prevSymbol}{vowel}";
-                //        text = NSub(text, note.tone);
-                //    }
-                //}
+                // vowel onset
+                var text = "";
+                if (prevSymbol == "-") {
+                    text = $"-{firstVowel}";
+                } else {
+                    if (isVowel.ContainsKey(prevSymbol) && isVowel[prevSymbol]) {
+                        // VV transition
+                        if (prevSymbol == firstVowel) {
+                            text = firstVowel;
+                        } else {
+                            text = $"{prevSymbol} {firstVowel}";
+                        }
+                    } else {
+                        text = $"{prevSymbol}{firstVowel}";
+                        text = NSub(text, note.tone);
+                    }
+                }
 
                 phonemes.Add(new Phoneme() {
-                    phoneme = firstVowel
+                    phoneme = text
                 });
             } else if (firstVowelPos == 1) {
+                // 1 consonant onset
                 var cv = $"{symbols[0]}{firstVowel}";
                 cv = NSub(cv, note.tone);
 
@@ -87,6 +97,7 @@ namespace OpenUtau.Plugin.Builtin {
                     phoneme = cv
                 });
             } else {
+                // cluster onset
                 //var prefix = prevSymbol == "-" ? "-" : "";
                 var cluster = "";
                 for (var i = 0; i < firstVowelPos; i++) {
@@ -124,67 +135,213 @@ namespace OpenUtau.Plugin.Builtin {
                 });
             }
 
+            var lastVowelPos = firstVowelPos;
             if (vowelPositions.Count() > 1) {
                 // more syllables
-            } else {
-                int noteLength = 0;
-                Array.ForEach(notes, i => noteLength += i.duration);
-
-                var remainder = symbols.Length - 1 - firstVowelPos;
-                if (remainder > 0) {
-                    if (remainder == 1) {
-                        var vc = $"{firstVowel}{symbols[firstVowelPos + 1]}";
-                        vc = NSub(vc, note.tone);
+                // each syllable centers around a vowel, go through list of vowel positions
+                for (var i = 1; i < vowelPositions.Count; i++) {
+                    var interval = vowelPositions[i] - lastVowelPos;
+                    var lastVow = symbols[lastVowelPos];
+                    var thisVow = symbols[vowelPositions[i]];
+                    var syllableStart = i * (noteLength / vowelPositions.Count);
+                    if (interval == 1) {
+                        // vowel onset
                         phonemes.Add(new Phoneme() {
-                            phoneme = vc,
-                            position = noteLength - Math.Min(120, noteLength / 2)
+                            phoneme = lastVow == thisVow ? thisVow : $"{lastVow} {thisVow}",
+                            position = syllableStart
+                        });
+                    } else if (interval == 2) {
+                        // 1 consonant onset
+                        var cons = symbols[vowelPositions[i] - 1];
+                        var vc = $"{lastVow} {cons}";
+                        vc = NSub(vc, note.tone);
+                        var cv = $"{cons}{thisVow}";
+                        cv = NSub(cv, note.tone);
+                        if(singer.TryGetMappedOto(cv, note.tone, out var oto)) {
+                            phonemes.Add(new Phoneme() {
+                                phoneme = vc,
+                                position = syllableStart - Math.Min(MsToTick(oto.Preutter),
+                                    ((noteLength / vowelPositions.Count) / 2))
+                            });
+                        }
+                        phonemes.Add(new Phoneme() {
+                            phoneme = cv,
+                            position = syllableStart
                         });
                     } else {
-                        var vc = $"{firstVowel} {symbols[firstVowelPos + 1]}";
-                        vc = NSub(vc, note.tone);
+                        // cluster onset
+                        
+                        var firstConPos = lastVowelPos + 1; // after the last vowel
+                        var firstCon = symbols[firstConPos];
+                        var lastConPos = vowelPositions[i] - 1; // before the current vowel
+                        var lastCon = symbols[lastConPos];
 
-                        var cluster = "";
-                        for (var i = firstVowelPos + 1; i < symbols.Length; i++) {
-                            cluster += symbols[i];
+                        var cluster = new List<string>();
+                        for (var j = firstConPos; j <= lastConPos; j++) {
+                            if (symbols[j] != symbols[j - 1]) {
+                                cluster.Add(NSub(symbols[j],note.tone));
+                            }
                         }
-                        cluster = NSub(cluster, note.tone);
+                        var clusterText = string.Join("", cluster);
 
-                        var vcLength = 120;
-                        var clusterLength = Math.Min(noteLength / 3, 120);
+                        if (singer.TryGetMappedOto(clusterText, note.tone, out var clusterOto)) {
+                            // dedicated cluster phoneme found
+                            var vc = $"{lastVow} {firstCon}";
+                            vc = NSub(vc, note.tone);
+                            var vcPos = 0;
 
-                        if (singer.TryGetMappedOto(cluster, note.tone, out var oto)) {
-                            vcLength = Math.Min(noteLength / 3, MsToTick(oto.Preutter));
+                            var clusterPos = 0;
+                            
+                            var cv = $"{lastCon}{thisVow}";
+                            cv = NSub(cv, note.tone);
+
+                            if (singer.TryGetMappedOto(cv, note.tone, out var cvOto)) {
+                                clusterPos = syllableStart - Math.Min(MsToTick(cvOto.Preutter),
+                                    ((noteLength / vowelPositions.Count) / 3));
+                                vcPos = clusterPos - Math.Min(MsToTick(clusterOto.Preutter),
+                                    ((noteLength / vowelPositions.Count) / 3));
+                            }
+
+                            phonemes.Add(new Phoneme() {
+                                phoneme = vc,
+                                position = vcPos
+                            });
+                            phonemes.Add(new Phoneme() {
+                                phoneme = clusterText,
+                                position = clusterPos
+                            });
+                            phonemes.Add(new Phoneme() {
+                                phoneme = cv,
+                                position = syllableStart
+                            });
+                        } else {
+                            // dedicated cluster phoneme not found, try to find 2 clusters
+                            var found = false;
+                            for (var j = 1; j < cluster.Count - 1; j++) {
+                                var cluster1 = string.Join("", cluster.GetRange(0, j + 1)); 
+                                var cluster2 = string.Join("", cluster.GetRange(j, cluster.Count-j));
+                                if (singer.TryGetMappedOto(cluster1, note.tone, out var c1oto)
+                                    && singer.TryGetMappedOto(cluster2, note.tone, out var c2oto)) {
+                                    found = true;
+
+                                    var vc = $"{lastVow} {firstCon}";
+                                    vc = NSub(vc, note.tone);
+                                    var vcPos = 0;
+
+                                    var c1pos = 0;
+                                    var c2pos = 0;
+
+                                    var cv = $"{lastCon}{thisVow}";
+                                    cv = NSub(cv, note.tone);
+
+                                    if (singer.TryGetMappedOto(cv, note.tone, out var cvOto)) {
+                                        c1pos = syllableStart - Math.Min(MsToTick(cvOto.Preutter),
+                                            ((noteLength / vowelPositions.Count) / 4));
+                                        c2pos = c1pos - Math.Min(MsToTick(c2oto.Preutter),
+                                            ((noteLength / vowelPositions.Count) / 4));
+                                        vcPos = c2pos - Math.Min(MsToTick(c1oto.Preutter),
+                                            ((noteLength / vowelPositions.Count) / 4));
+                                    }
+
+                                    phonemes.Add(new Phoneme() {
+                                        phoneme = vc,
+                                        position = vcPos
+                                    });
+                                    phonemes.Add(new Phoneme() {
+                                        phoneme = cluster1,
+                                        position = c1pos
+                                    });
+                                    phonemes.Add(new Phoneme() {
+                                        phoneme = cluster2,
+                                        position = c2pos
+                                    });
+                                    phonemes.Add(new Phoneme() {
+                                        phoneme = cv,
+                                        position = syllableStart
+                                    });
+                                }
+                            }
+
+                            if (!found) {
+                                // give up, only use 2 consonants
+                                var vc = $"{lastVow}{firstCon}";
+                                vc = NSub(vc, note.tone);
+                                phonemes.Add(new Phoneme() {
+                                    phoneme = vc,
+                                    position = syllableStart - Math.Min(120, 
+                                        ((noteLength / vowelPositions.Count) / 2))
+                                });
+
+                                var cv = $"{lastCon}{thisVow}";
+                                cv = NSub(cv, note.tone);
+                                phonemes.Add(new Phoneme() {
+                                    phoneme = cv,
+                                    position = syllableStart
+                                });
+                            }
                         }
-
-                        phonemes.Add(new Phoneme() {
-                            phoneme = vc,
-                            position = noteLength - clusterLength - vcLength
-                        });
-                        phonemes.Add(new Phoneme() {
-                            phoneme = cluster,
-                            position = noteLength - clusterLength
-                        });
                     }
+                    lastVowelPos = vowelPositions[i];
                 }
-                //else {
-                //    // vowel coda
-                //    if (nextSymbol == "-") {
-                //        phonemes.Add(new Phoneme() {
-                //            phoneme = $"{symbols[firstVowel]}-",
-                //            position = noteLength - 120
-                //        });
-                //    } else if (!isVowel[nextSymbol]) {
-                //        // insert vc??
-                //    }
-                //}
             }
+
+            var lastVowel = symbols[lastVowelPos];
+
+            // Final coda
+            var remainder = symbols.Length - 1 - lastVowelPos;
+            if (remainder > 0) {
+                if (remainder == 1) {
+                    var vc = $"{lastVowel}{symbols[lastVowelPos + 1]}";
+                    vc = NSub(vc, note.tone);
+                    phonemes.Add(new Phoneme() {
+                        phoneme = vc,
+                        position = noteLength - Math.Min(120, noteLength / 2)
+                    });
+                } else {
+                    var vc = $"{lastVowel} {symbols[lastVowelPos + 1]}";
+                    vc = NSub(vc, note.tone);
+
+                    var cluster = "";
+                    for (var i = lastVowelPos + 1; i < symbols.Length; i++) {
+                        cluster += symbols[i];
+                    }
+                    cluster = NSub(cluster, note.tone);
+
+                    var vcLength = 120;
+                    var clusterLength = Math.Min(noteLength / 3, 120);
+
+                    if (singer.TryGetMappedOto(cluster, note.tone, out var oto)) {
+                        vcLength = Math.Min(noteLength / 3, MsToTick(oto.Preutter));
+                    }
+
+                    phonemes.Add(new Phoneme() {
+                        phoneme = vc,
+                        position = noteLength - clusterLength - vcLength
+                    });
+                    phonemes.Add(new Phoneme() {
+                        phoneme = cluster,
+                        position = noteLength - clusterLength
+                    });
+                }
+            }
+            //else {
+            //    // vowel coda
+            //    if (nextSymbol == "-") {
+            //        phonemes.Add(new Phoneme() {
+            //            phoneme = $"{symbols[firstVowel]}-",
+            //            position = noteLength - 120
+            //        });
+            //    } else if (!isVowel[nextSymbol]) {
+            //        // insert vc??
+            //    }
+            //}
 
             return new Result {
                 phonemes = phonemes.ToArray()
             };
         }
 
-        // Bool to signify vowels
+        // Valid phonemes and whether or not they're vowels
         private Dictionary<string, bool> isVowel = new Dictionary<string, bool> {
             {"a", true},
             {"i", true},
@@ -233,6 +390,7 @@ namespace OpenUtau.Plugin.Builtin {
             {"N", false}
         };
 
+        // Mapping from ARPAABET to Grayglish subset of X-SAMPA
         private Dictionary<string, string> symbolMap = new Dictionary<string, string> {
             {"aa", "a"},
             {"ae", "{"},
@@ -279,14 +437,17 @@ namespace OpenUtau.Plugin.Builtin {
 
         private string[] GetSymbols(Note note) {
             if (string.IsNullOrEmpty(note.phoneticHint)) {
+                // Hint not available, check cmudict
                 var arpabet = cmudict.Query(note.lyric.ToLowerInvariant());
                 if (arpabet == null) {
                     return null;
                 }
+                // Remapping from arpabet to reclist phonemes
                 var converted = new List<string>();
                 for (var i = 0; i < arpabet.Length; i++) {
                     var symbol = arpabet[i];
                     if (symbol == "ae" && (arpabet[i+1] == "n" || arpabet[i+1] == "m")) {
+                        // Special case for nasal ae
                         converted.Add("e@");
                     } else {
                         converted.Add(symbolMap[symbol]);
@@ -294,11 +455,13 @@ namespace OpenUtau.Plugin.Builtin {
                 }
                 return converted.ToArray();
             }
+            // Hint available, just use the hint
             return note.phoneticHint.Split()
-                .Where(s => isVowel.ContainsKey(s))
+                .Where(s => isVowel.ContainsKey(s)) // Filter hint to only contain valid phonemes
                 .ToArray();
         }
-    
+        
+        // Grayglish doesn't have full CVVC for the N phoneme, suggested sub with n instead
         private string NSub(string phoneme, int tone) {
             if (phoneme.IndexOf("N") > -1) {
                 if (!singer.TryGetMappedOto(phoneme, tone, out var _)) {
