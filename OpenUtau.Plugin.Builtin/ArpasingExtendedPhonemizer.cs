08d9036ffba37548342182ec2c6e4097585c6f0c
Mark Beltran <markmbeltran@gmail.com>
2022-10-16 21:03:26 +0800

    Basic ARPAsing phonemizer

---
+++ OpenUtau.Plugin.Builtin/ArpasingExtendedPhonemizer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenUtau.Api;
+using OpenUtau.Core.G2p;
+using OpenUtau.Core.Ustx;
+
+namespace OpenUtau.Plugin.Builtin {
+    [Phonemizer("ARPAsing Extended Phonemizer", "EN ARPA-X", "TUBS", language: "EN")]
+    public class ArpasingExtendedPhonemizer : SyllableBasedPhonemizer {
+        protected IG2p g2p;
+        public ArpasingExtendedPhonemizer() {
+            g2p = new ArpabetG2p();
+        }
+
+        public override void SetSinger(USinger singer) {
+            this.singer = singer;
+            // load singer g2p stuff here
+        }
+
+        protected override string[] GetVowels() => vowels;
+        private static readonly string[] vowels =
+            "aa ae ah ao eh er ih iy uh uw ay ey oy ow aw ax".Split();
+
+        protected override string[] GetSymbols(Note note) {
+            if (string.IsNullOrEmpty(note.phoneticHint)) {
+                return g2p.Query(note.lyric.ToLowerInvariant());
+            } else {
+                return note.phoneticHint.Split(" ");
+            }
+        }
+
+        protected override List<string> ProcessSyllable(Syllable syllable) {
+            if (CanMakeAliasExtension(syllable)) {
+                return new List<string> { "null" };
+            }
+
+            var phonemes = new List<string>();
+            var symbols = new List<string>();
+            symbols.Add(syllable.prevV == "" ? "-" : syllable.prevV);
+            symbols.AddRange(syllable.cc);
+            symbols.Add(syllable.v);
+
+            for (int i = 0; i < symbols.Count-1; i++) {
+                phonemes.Add($"{symbols[i]} {symbols[i+1]}");
+            }
+            return phonemes;
+        }
+
+        protected override List<string> ProcessEnding(Ending ending) {
+            var phonemes = new List<string>();
+            var symbols = new List<string>();
+            symbols.Add(ending.prevV);
+            symbols.AddRange(ending.cc);
+            symbols.Add("-");
+
+            for (int i = 0; i < symbols.Count - 1; i++) {
+                phonemes.Add($"{symbols[i]} {symbols[i + 1]}");
+            }
+            return phonemes;
+        }
+    }
+}
---
+++ OpenUtau.Test/Plugins/EnArpaXTest.cs
@@ -0,0 +1,37 @@
+﻿using OpenUtau.Api;
+using OpenUtau.Plugin.Builtin;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace OpenUtau.Plugins {
+    public class EnArpaXTest : PhonemizerTestBase {
+        public EnArpaXTest(ITestOutputHelper output) : base(output) { }
+        protected override Phonemizer CreatePhonemizer() {
+            return new ArpasingExtendedPhonemizer();
+        }
+
+        [Theory]
+        // Basic ARPAsing
+        [InlineData("en_arpa_x",
+            new string[] { "test", "words"},
+            new string[] { "C3", "C3" },
+            new string[] { "", "",},
+            new string[] { "- t", "t eh", "eh s", "s t", "t w", "w er", "er d", "d z", "z -"})]
+        // Basic ARPAsing multipitch
+        [InlineData("en_arpa_x",
+            new string[] { "test", "words" },
+            new string[] { "C3", "C4" },
+            new string[] { "", "", },
+            new string[] { "- t", "t eh", "eh s", "s t", "t w", "w er_H", "er d_H", "d z_H", "z -_H" })]
+        // Basic ARPAsing voice colors
+        // (Colors are correctly applied to each phoneme in manual testing)
+        [InlineData("en_arpa_x",
+            new string[] { "test", "words" },
+            new string[] { "C3", "C3" },
+            new string[] { "", "Power", },
+            new string[] { "- t", "t eh", "eh s_P", "s t", "t w", "w er", "er d", "d z", "z -" })]
+        public void PhonemizeTest(string singerName, string[] lyrics, string[] tones, string[] colors, string[] aliases) {
+            RunPhonemizeTest(singerName, lyrics, tones, colors, aliases);
+        }
+    }
+}
